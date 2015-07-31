159948af63e28cad60dea24817fabf6d21cdff37
belhard-user <ldarik2l@gmail.com>
2015-07-31 19:06:00 +0300

    pass variable to view

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -11,6 +11,18 @@ class ViewController extends Controller
 {
     public function index()
     {
-        
+        $names = [
+            'Neo',
+            'Morpheus',
+            'Trinity',
+            'Tank',
+            'John Smith'
+        ];
+
+        array_push($names, 'Mike');
+
+        $header = "People i like";
+
+        return view('templates.layouts', compact('names', 'header'));
     }
 }
